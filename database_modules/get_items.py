dfe6ed91e5db93096c14ae42b0b3dd31f165aade
Isak-Landin <utbisalan@gmail.com>
2023-06-17 05:25:53 +0200

    Create get_items.py

---
+++ database_modules/get_items.py
@@ -0,0 +1,27 @@
+import requests
+from database_modules.localconfig import astra_id, headers
+from flask import make_response, Response
+
+
+keyspace = 'restapi'
+table = 'items'
+
+
+def get_item(store, category, uuid):
+    url = f'https://{astra_id}-europe-west1.apps.astra.datastax.com/api/rest/v1/keyspaces/{keyspace}/tables/{table}/rows/{store};{category};{uuid}'
+    return requests.get(url, headers=headers)
+
+
+def get_items():
+    url = f'https://{astra_id}-europe-west1.apps.astra.datastax.com/api/rest/v1/keyspaces/{keyspace}/tables/{table}/rows'
+    response = requests.get(url, headers=headers)
+    try:
+        flask_response = make_response(response.content)
+        flask_response.status_code = response.status_code
+        flask_response.headers['Content-Type'] = response.headers.get('Content-Type')
+    except Exception as e:
+        print(e)
+        flask_response = Response()
+
+    return flask_response
+
