e632085f4b3cc1fc8bf4a6386f59fdc346166c7b
Isak-Landin <utbisalan@gmail.com>
2023-06-22 00:18:50 +0200

    Update server_stability.py

--- server_stability.py
+++ server_stability.py
@@ -1,4 +1,15 @@
 class ServerStatus:
     module_statuses = {
-
+        'datastax-connection': False,
+        'session-manager': False,
+        'jwt-manager': False
     }
+
+    @classmethod
+    def is_stable(cls):
+        all_pairs = cls.module_statuses.items()
+        print(all_pairs)
+        return all_pairs
+
+
+all_pairs_retrieved = ServerStatus.is_stable()
