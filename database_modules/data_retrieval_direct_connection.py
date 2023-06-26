e1fe09f1c1e46af88528e8251d4edb8b24f952f4
Isak-Landin <utbisalan@gmail.com>
2023-06-27 01:13:57 +0200

    Update data_retrieval_direct_connection.py

--- database_modules/data_retrieval_direct_connection.py
+++ database_modules/data_retrieval_direct_connection.py
@@ -66,64 +66,19 @@ class DatastaxDataRetrieval:
         """
 
         def convert_to_json():
-            def get_columns():
-                if single_row is None and cluster is None:
-                    raise AttributeError('Was expecting either single row or cluster.')
-                elif single_row and cluster:
-                    raise AttributeError('Was not expecting both single row and cluster')
-
-                columns = None
-                if single_row:
-                    # noinspection PyBroadException
-                    try:
-                        original_row_string: str = single_row.__doc__
-                        new_string = original_row_string.replace('Row', '').replace(',', '').replace('(', '').replace(
-                            ')',
-                            '')
-                        columns = new_string.split(' ')
-                        print(columns)
-                    except TypeError:
-                        print('TypeError event')
-                    except IndexError:
-                        print('IndexError event')
-                    except Exception:
-                        print('Exception event')
-                elif cluster:
-                    # noinspection PyBroadException
-                    try:
-                        one_row = cluster.one()
-                        original_row_string: str = one_row.__doc__
-                        new_string = original_row_string.replace('Row', '').replace(',', '').replace('(', '').replace(
-                            ')',
-                            '')
-                        columns = new_string.split(' ')
-                        print(columns)
-                    except TypeError:
-                        print('TypeError event')
-                    except IndexError:
-                        print('IndexError event')
-                    except AttributeError:
-                        print('AttributeError event')
-                    except Exception:
-                        print('Exception event')
-
-                if columns:
-                    return columns
-
-            _columns = get_columns()
+            """_columns = get_columns()
             if _columns:
-                _columns_max_index = len(_columns) - 1
+                _columns_max_index = len(_columns) - 1"""
 
             dict_key_values_from_row_or_cluster = {}
 
-            if cluster and not single_row and _columns:
-                int_key = 1
+            if cluster and not single_row:
 
                 for row in cluster:
                     row_as_dict = row._asdict()
 
                     dict_key_values_from_row_or_cluster = {**dict_key_values_from_row_or_cluster, **row_as_dict}
-            elif single_row and not cluster and _columns:
+            elif single_row and not cluster:
                 row_as_dict = single_row._asdict()
             else:
                 pass
