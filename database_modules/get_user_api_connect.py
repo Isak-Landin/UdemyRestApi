f690d6bc0440f78db76eba026299a99a547a5292
Isak-Landin <utbisalan@gmail.com>
2023-07-03 01:27:46 +0200

    Update get_user_api_connect.py

--- database_modules/get_user_api_connect.py
+++ database_modules/get_user_api_connect.py
@@ -1,12 +1,14 @@
+import json
+
 from database_modules.localconfig import table_users, keyspace, admin_headers, astra_id
+from database_modules.utils import response_decoder
 import requests
 from flask import make_response, Response
 
 
-def get_user(username, jwt_access=None, jwt_refresh=None):
-    if jwt_access and jwt_refresh:
-        # Add todo if necessary
-        pass
-    url = f'https://{astra_id}-europe-west1.apps.astra.datastax.com/api/rest/v1/keyspaces/{keyspace}/tables/{table_users}/rows/{username}'
+def get_user_and_password(username):
+    url = f'https://{astra_id}-europe-west1.apps.astra.datastax.com/api/rest/v2/keyspaces/{keyspace}/{table_users}/{username}?fields=username%2C%20password'
     return requests.get(url, headers=admin_headers)
 
+
+print(response_decoder(get_user_and_password('user4'))['data'][0])
