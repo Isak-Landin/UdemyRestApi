4459e05bf162803b72bd58ffa61b7f0853c43012
Isak-Landin <utbisalan@gmail.com>
2023-07-06 07:02:27 +0200

    Update utils.py

--- database_modules/utils.py
+++ database_modules/utils.py
@@ -6,6 +6,18 @@ def response_decoder(response: requests.Response) -> dict:
     return json.loads(response.content.decode('utf-8'))
 
 
+def get_count_in_dict(decoded_dict: dict):
+    count = None
+    try:
+        count = decoded_dict.get('count')
+        if not count:
+            raise KeyError
+    except (KeyError, AttributeError):
+        return {}
+
+    return count
+
+
 def get_data_in_dict(decoded_dict: dict):
     data = None
     try:
