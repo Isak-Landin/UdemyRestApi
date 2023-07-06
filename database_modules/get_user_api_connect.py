75d352d22aab3d161698865deb9caa405c479312
Isak-Landin <utbisalan@gmail.com>
2023-07-06 11:23:03 +0200

    Update get_user_api_connect.py

--- database_modules/get_user_api_connect.py
+++ database_modules/get_user_api_connect.py
@@ -7,5 +7,5 @@ from flask import make_response, Response
 
 
 def get_user_and_password(username):
-    url = f'https://{astra_id}-europe-west1.apps.astra.datastax.com/api/rest/v2/keyspaces/{keyspace}/{table_users}/{username}?fields=username%2C%20password'
+    url = f'https://{astra_id}-europe-west1.apps.astra.datastax.com/api/rest/v2/keyspaces/{keyspace}/{table_users}/{username}?fields=username%2C%20password%2C%20salt'
     return requests.get(url, headers=admin_headers)
