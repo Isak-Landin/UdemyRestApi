7f1f61d216b2741ffc1f5376fd6b79cc3ff67091
Isak-Landin <utbisalan@gmail.com>
2023-07-06 07:02:32 +0200

    Update validate_user.py

--- database_modules/validate_user.py
+++ database_modules/validate_user.py
@@ -1,30 +1,69 @@
 import requests
-from database_modules.get_user_api_connect import get_user
+
+from database_modules.get_user_api_connect import get_user_and_password
+from database_modules.localconfig import astra_id, keyspace, table_users
+from database_modules.utils import response_decoder, get_data_in_dict, get_rows_in_dict, get_count_in_dict
+
+import bcrypt
 import json
 import contextlib
 
 
-def validate_user(username: str, password: str, secret=None) -> bool:
-    is_user_response = get_user(username=username)
+def validate_user(username: str, password: str) -> bool:
+    get_user_response = get_user_and_password(username=username)
 
-    if is_user_exist_in_response(is_user_response=is_user_response):
-        is_correct_password(password=password)
+    if isinstance(get_user_response, requests.Response) and\
+            is_user_exist_in_response(get_user_response=get_user_response):
+        return is_correct_password(input_username=username, input_password=password, response=get_user_response)
+
+    return False
 
 
-def is_user_exist_in_response(is_user_response: requests.Response) -> bool:
+def is_user_exist_in_response(get_user_response: requests.Response) -> bool:
     is_username = False
-    if is_user_response:
-        with contextlib.suppress(AttributeError, TypeError, KeyError):
-            content_decoded = json.loads(is_user_response.content.decode('utf-8'))
-            count = content_decoded['count']
+    if get_user_response:
+        content_decoded = response_decoder(get_user_response)
+        count = get_count_in_dict(content_decoded)
+        try:
             if count > 0:
                 is_username = True
+        except TypeError:
+            with contextlib.suppress(TypeError):
+                if int(count) > 0:
+                    is_username = True
 
     return is_username
 
 
-def is_correct_password(password: str):
-    pass
+def is_correct_password(input_username: str, input_password: str, response: requests.Response) -> bool:
+    decoded_response = response_decoder(response=response)
+    decoded_data = get_data_in_dict(decoded_response)
 
-test_value_dict = validate_user(username='test', password='test')
+    def strip_password():
+        if decoded_data:
+            password = decoded_data.get('password')
+            if password:
+                return password
 
+        return ''
+
+    def strip_username():
+        if decoded_data:
+            username = decoded_data.get('username')
+            if username:
+                return username
+
+        return ''
+
+    def bcrypt_match_passwords(stripped_response_password: str):
+        if not bool(stripped_response_password):
+            return False
+        return bcrypt.checkpw(input_password.encode(), stripped_response_password.encode())
+
+    stripped_password = strip_password()
+    if strip_username() == input_username:
+        return bcrypt_match_passwords(stripped_response_password=stripped_password)
+
+    return False
+
+validate_user()
