187c765efc9cecf480bb23058bbe07b9da2bc205
Isak-Landin <utbisalan@gmail.com>
2023-07-10 01:06:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 from datetime import timedelta
 
-import contextlib
-
-import flask
-
-from database_modules.get_items_api_connect import get_items as get_items_func
 from App_Config.app_config import jwt_secret
 from server_stability import ServerStatus
-from database_modules.validate_user import validate_user
 
-from flask import Flask, jsonify, request, abort, Response
-
-from flask_jwt_extended import create_access_token, create_refresh_token
-from flask_jwt_extended import jwt_required, get_jwt, get_jwt_identity
+from flask import Flask, abort
+from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
-from flask_jwt_extended import set_access_cookies
-from flask_jwt_extended import unset_jwt_cookies
 
-app = Flask(__name__)
+from views.auth.routes import auth_blueprint as auth_bp
+from views.items.routes import items_blueprint as items_bp
+from views.stores.routes import stores_blueprint as stores_bp
+
+
+def create_app():
+    _app = Flask(__name__)
+    _app.register_blueprint(auth_bp)
+    _app.register_blueprint(items_bp)
+    _app.register_blueprint(stores_bp)
+    return _app
+
+
+app = create_app()
 
 app.config['JWT_SECRET_KEY'] = jwt_secret
 app.config['JWT_TOKEN_LOCATION'] = ['headers', 'cookies']
@@ -35,75 +38,4 @@ def is_server_stable():
         abort(500)
 
 
-@app.route('/login', methods=['POST'])
-def login():
-    if request.method == 'GET':
-        return abort(401)
-    request_content_as_json: dict = request.get_json()
-    request_headers_as_dict = dict(request.headers)
-    content_type = request_headers_as_dict.get('Content-Type')
 
-    response = flask.Response()
-
-    def login_with_cookies(_username, _password, headers, json=None):
-        return False
-
-    def login_without_cookies(_username, _password, headers=None, json=None):
-        is_valid_user = validate_user(username=str(_username), password=str(_password))
-
-        # Create logic that fits with JWTManager and Session
-        if is_valid_user:
-            __access_token = create_access_token(identity=_username)
-            __refresh_token = create_refresh_token(identity=_username)
-        else:
-            __access_token = None
-            __refresh_token = None
-
-        return __access_token, __refresh_token
-
-    username = request_content_as_json.get('username')
-    password = request_content_as_json.get('password')
-    access = request_headers_as_dict.get('Authorization')
-
-    if username and password:
-        access_token, refresh_token = login_without_cookies(_username=username, _password=password)
-        if access_token and refresh_token:
-            response = jsonify(access_token=access_token, refresh_token=refresh_token)
-            response.status_code = 200
-            response.headers['Content-Type'] = 'application/json'
-
-            return response
-    elif access:
-        login_with_cookies()
-    else:
-        return jsonify({'msg': 'Invalid or no credentials'})
-
-    return jsonify(message='Invalid credentials'), 401
-
-
-@app.route('/item', methods=['POST', 'GET'])
-@jwt_required()
-def get_item():
-    return {'State': 'Work in progress'}
-
-
-@app.route('/items', methods=['POST', 'GET'])
-@jwt_required()
-def get_items():
-    return get_items_func()
-
-
-@app.route('/store', methods=['POST', 'GET'])
-@jwt_required()
-def get_store():
-    return {'State': 'Work in progress'}
-
-
-@app.route('/stores', methods=['POST', 'GET'])
-@jwt_required()
-def get_stores():
-    return {'State': 'Work in progress'}
-
-@app.route('/refresh', methods=['POST', 'GET'])
-@jwt_required(refresh=True)
-def refresh():
