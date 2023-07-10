e17ad62d1424957dc32feb24d2803f81cdaefc41
Isak-Landin <utbisalan@gmail.com>
2023-07-10 22:58:44 +0200

    Create token_managing.py

---
+++ views/auth/token_managing.py
@@ -0,0 +1,10 @@
+from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import get_jwt_identity
+
+
+class TokenManager:
+    @staticmethod
+    def refresh_refresh_token(refresh_token):
+        pass
+
+    @jwt
