951ab5391545cb37260a09b459dcb9609ca0a23a
Isak-Landin <utbisalan@gmail.com>
2023-07-16 01:02:04 +0200

    Update get_user_api_connect.py

--- database_modules/get_user_api_connect.py
+++ database_modules/get_user_api_connect.py
@@ -1,11 +1,21 @@
 import json
 
-from database_modules.localconfig import table_users, keyspace, admin_headers, astra_id
+from database_modules.localconfig import table_users, table_emails, keyspace, admin_headers, astra_id
 from database_modules.utils import response_decoder
 import requests
 from flask import make_response, Response
 
 
 def get_user_and_password(username):
-    url = f'https://{astra_id}-europe-west1.apps.astra.datastax.com/api/rest/v2/keyspaces/{keyspace}/{table_users}/{username}?fields=username%2C%20password%2C%20salt'
+    url = f'https://{astra_id}-europe-west1.apps.astra.datastax.com/api/rest/v2/keyspaces/{keyspace}/{table_users}/{username}?fields=username%2C%20password'
+    return requests.get(url, headers=admin_headers)
+
+
+def get_user(username):
+    url = f'https://{astra_id}-europe-west1.apps.astra.datastax.com/api/rest/v2/keyspaces/{keyspace}/{table_users}/{username}?fields=username'
+    return requests.get(url, headers=admin_headers)
+
+
+def get_email(email):
+    url = f'https://{astra_id}-europe-west1.apps.astra.datastax.com/api/rest/v2/keyspaces/{keyspace}/{table_users}/{email}?fields=username%2C%20password%2C%20salt'
     return requests.get(url, headers=admin_headers)
