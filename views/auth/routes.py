9f42319c84a9e27b59a8cc952fdfd80d2ffc3221
Isak-Landin <utbisalan@gmail.com>
2023-07-21 00:18:06 +0200

    Update routes.py

--- views/auth/routes.py
+++ views/auth/routes.py
@@ -6,12 +6,12 @@ from JWTManagement.TokenGeneration import Tokens
 auth_blueprint = Blueprint('auth', __name__)
 
 
-def login_with_cookies(_username, _password, headers, json=None):
+def login_with_tokens(_username, _password, access_token, refresh_token):
     # Future code here
     return False
 
 
-def login_without_cookies(_username, _password, headers=None, json=None):
+def login_without_tokens(_username, _password):
     if tokens := Tokens.generate_token_pair(
         username=str(_username), password=str(_password)
     ):
@@ -33,18 +33,19 @@ def login():
     password = request_content_as_json.get('password')
 
     request_headers_as_dict = dict(request.headers)
-    access = request_headers_as_dict.get('Authorization')
+    access_token = request_headers_as_dict.get('Authorization')
+    refresh_token = request_headers_as_dict.get('Authorization-Refresh')
     content_type = request_headers_as_dict.get('Content-Type')  # for future use
 
-    if username and password:
-        access_token, refresh_token = login_without_cookies(_username=username, _password=password)
+    if username and password and not access_token and not refresh_token:
+        access_token, refresh_token = login_without_tokens(_username=username, _password=password)
         if access_token and refresh_token:
             response = jsonify(access_token=access_token, refresh_token=refresh_token)
             response.status_code = 200
             response.headers['Content-Type'] = 'application/json'
             return response
-    elif access:
-        login_with_cookies(username, password, request_headers_as_dict, request_content_as_json)
+    elif access_token and refresh_token:
+        login_with_tokens(username, password, access_token, refresh_token)
     else:
         return jsonify({'msg': 'Invalid or no credentials'}), 401
 
