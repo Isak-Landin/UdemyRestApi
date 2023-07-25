21a552a524720d5e35ed4c23b9de52bd5a7fd94c
Isak-Landin <utbisalan@gmail.com>
2023-07-25 08:37:18 +0200

    Update utils.py

--- database_modules/utils.py
+++ database_modules/utils.py
@@ -1,16 +1,18 @@
 import json
 import requests
+from requests import Response
 from GeneralUtils.decorators import reformatting
 
 
 @reformatting
-def response_decoder(response: requests.Response) -> dict:
+def response_content_decoder(response: Response) -> dict:
     return json.loads(response.content.decode('utf-8'))
 
 
 @reformatting
 def get_count_in_dict(decoded_dict: dict):
-    return count if (count := decoded_dict.get('count')) else {}
+    count = decoded_dict.get('count')
+    return count if count or count == 0 else {}
 
 
 @reformatting
@@ -23,4 +25,9 @@ def get_data_in_dict(decoded_dict: dict):
 def get_rows_in_dict(decoded_dict: dict):
     rows = decoded_dict.get('rows', [])
     return rows[0] or {}
+
+
+@reformatting
+def get_status_code_of_response(response: dict):
+    return response.status_code if isinstance(response, Response) else None
 
