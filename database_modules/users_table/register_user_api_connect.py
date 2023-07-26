8b2d4b183610d6b183dbb021c4abd854a8bb47c9
Isak-Landin <utbisalan@gmail.com>
2023-07-26 08:20:01 +0200

    Create register_user_api_connect.py

---
+++ database_modules/users_table/register_user_api_connect.py
@@ -0,0 +1,22 @@
+from database_modules.users_table.get_user_api_connect import get_user
+from database_modules.mails_table.get_mail_api_connect import get_email
+from database_modules.utils import response_content_decoder, get_count_in_dict, get_status_code_of_response
+
+
+def save_user_to_database(username, email):
+    if not is_username_or_email_exists(username, email):
+        pass
+
+
+def is_username_or_email_exists(username, email) -> bool:
+    retrieved_email_response = get_email(email=email)
+    retrieved_username_response = get_user(username=username)
+
+    if get_status_code_of_response(retrieved_username_response) == 200 and get_status_code_of_response(retrieved_email_response) == 200:
+        is_username = get_count_in_dict(response_content_decoder(retrieved_username_response))
+        is_email = get_count_in_dict(response_content_decoder(retrieved_email_response))
+
+        if is_username == 0 and is_email == 0:
+            return False
+
+    return True
