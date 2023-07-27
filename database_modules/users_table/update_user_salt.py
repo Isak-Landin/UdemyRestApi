2e063593930ffb9639d33266a5a6ab9ccc0760d2
Isak-Landin <utbisalan@gmail.com>
2023-07-28 00:28:06 +0200

    Update update_user_salt.py

--- database_modules/users_table/update_user_salt.py
+++ database_modules/users_table/update_user_salt.py
@@ -3,15 +3,14 @@ import bcrypt
 from database_modules.localconfig import astra_id, table_users, keyspace
 
 
-def update_salt(username):
-    new_token_salt = bcrypt.gensalt().decode('utf-8')
+def update_salt(username, salt):
     url = f'{astra_id}-europe-west1.apps.astra.datastax.com/api/rest/v1/keyspaces/{keyspace}/tables/{table_users}/rows/{username}'
 
     body = {
         "changeset": [
             {
-                "column": "token_salt",
-                "value": f"{new_token_salt}"
+                "column": "token-salt",
+                "value": f"{salt}"
             }
         ]
     }
