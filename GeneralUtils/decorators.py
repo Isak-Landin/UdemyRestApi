b56a7dfb8eab9efd92ea4f75852bbcde464a8f22
Isak-Landin <utbisalan@gmail.com>
2023-07-28 00:27:49 +0200

    Update decorators.py

--- GeneralUtils/decorators.py
+++ GeneralUtils/decorators.py
@@ -1,15 +1,33 @@
 from functools import wraps
-from flask import jsonify
-from flask import Response
+
+import flask
+from flask import jsonify, Response
+from flask import request
+import traceback
+
+from flask_socketio import SocketIO, emit
 
 
-def reformatting(function):
+
+def standard_procedure(function):
     @wraps(function)
-    def general_exception_message_event(*args, **kwargs):
+    def standard_exception_return_event(*args, **kwargs):
         try:
+            for arg in args:
+                if isinstance(arg, flask.Response):
+                    return arg
+            for key, value in kwargs.items():
+                if isinstance(value, Response):
+                    return value
             return function(*args, **kwargs)
         except Exception as e:
-            error_response = {'error': str(e)}
+            error_response = {
+                'error': 'There was a server error that prevented your request from executing as expected. The error has been logged and we are working on a fix this very moment.',
+                'direct-error': str(e)
+            }
+            print(traceback.format_exc())
+            # TODO store this error in a database, figure out what columns are needed, pk and order to be stored
+
             return jsonify(error_response), 500
 
-    return general_exception_message_event
+    return standard_exception_return_event
