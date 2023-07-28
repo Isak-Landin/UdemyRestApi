3376e686980976d919847e54da6edef737c82b18
Isak-Landin <utbisalan@gmail.com>
2023-07-28 02:24:18 +0200

    Update register_new_user.py

---
+++ views/register/register_new_user.py
@@ -0,0 +1,12 @@
+from views.register.new_user_preparation import create_template
+from GeneralUtils.decorators import standard_procedure
+
+
+@standard_procedure
+def finalize_and_store_new_user():
+    pass
+
+
+@standard_procedure
+def perform_initial_checks():
+    pass
