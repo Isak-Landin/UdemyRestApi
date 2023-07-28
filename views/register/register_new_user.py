b5b3e37a4991b09defc8ccf7d1cc48f90b98b65c
Isak-Landin <utbisalan@gmail.com>
2023-07-28 23:01:35 +0200

    Update register_new_user.py

--- views/register/register_new_user.py
+++ views/register/register_new_user.py
@@ -1,11 +1,14 @@
+"""Flask Modules"""
 import flask
+from flask import Response
 
-from database_modules.users_table import get_user_api_connect
+"""Database Modules"""
+from database_modules.users_table.get_user_api_connect import get_user
 
+"""Other Modules"""
 from views.register.new_user_preparation import create_template
 from GeneralUtils.decorators import standard_procedure
-
-from flask import Response
+from database_modules.utils import response_content_decoder, get_count_in_dict
 
 
 def finalize_and_store_new_user() -> Response:
@@ -17,19 +20,29 @@ def finalize_and_store_new_user() -> Response:
 
 
 @standard_procedure
-def perform_initial_checks() -> bool:
+def perform_initial_checks(username, mail) -> bool:
     """
     :return: the returned bool will represent whether it's a green light.
     can the user be registered without collisions?
     :rtype: bool:
     """
+
+    is_username_available = check_username_available(username)
     return False
 
 
 @standard_procedure
 def check_username_available(username: str):
-    get_user_api_connect.get_user()
+    username_in_database_response = get_user(username)
+    decoded_response_content = response_content_decoder(username_in_database_response)
+    count = get_count_in_dict(decoded_response_content)
 
+    return count == 0
+
+
+@standard_procedure
+def check_mail_available(mail):
+    mail_in_database_response =
 @standard_procedure
 def create_template_and_user():
     pass
