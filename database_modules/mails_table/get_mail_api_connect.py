de00c11d5df28507bf524a452cde2c6fb762c094
Isak-Landin <utbisalan@gmail.com>
2023-07-30 00:22:36 +0200

    Update get_mail_api_connect.py

--- database_modules/mails_table/get_mail_api_connect.py
+++ database_modules/mails_table/get_mail_api_connect.py
@@ -1,7 +1,9 @@
 import requests
 from database_modules.localconfig import astra_id, admin_headers, keyspace, table_emails
 
+from GeneralUtils.decorators import request_procedure
 
+@request_procedure
 def get_email(email):
     url = f'https://{astra_id}-europe-west1.apps.astra.datastax.com/api/rest/v2/keyspaces/{keyspace}/{table_emails}/{email}'
     return requests.get(url, headers=admin_headers)
