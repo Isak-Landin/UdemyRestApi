4e7dff614b1b5b9bf4428ff1735c45f1aa49c95f
Isak-Landin <utbisalan@gmail.com>
2023-07-29 04:28:42 +0200

    Update decorators.py

--- GeneralUtils/decorators.py
+++ GeneralUtils/decorators.py
@@ -1,33 +1,17 @@
 from functools import wraps
-
-import flask
-from flask import jsonify, Response
-from flask import request
 import traceback
-
-from flask_socketio import SocketIO, emit
-
-
 
 def standard_procedure(function):
     @wraps(function)
     def standard_exception_return_event(*args, **kwargs):
         try:
-            for arg in args:
-                if isinstance(arg, flask.Response):
-                    return arg
-            for key, value in kwargs.items():
-                if isinstance(value, Response):
-                    return value
             return function(*args, **kwargs)
         except Exception as e:
-            error_response = {
-                'error': 'There was a server error that prevented your request from executing as expected. The error has been logged and we are working on a fix this very moment.',
-                'direct-error': str(e)
-            }
             print(traceback.format_exc())
-            # TODO store this error in a database, figure out what columns are needed, pk and order to be stored
-
-            return jsonify(error_response), 500
-
+            raise e
     return standard_exception_return_event
+
+
+def request_procedure(function):
+    @wraps(function)
+    def standar
