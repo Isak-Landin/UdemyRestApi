f9e89ca05f32aff352f750fe190f7f6da004b4c7
Isak-Landin <utbisalan@gmail.com>
2023-07-30 00:57:47 +0200

    Update get_user_salt.py

--- database_modules/users_table/get_user_salt.py
+++ database_modules/users_table/get_user_salt.py
@@ -1,7 +1,10 @@
 import requests
 from database_modules.localconfig import astra_id, keyspace, table_users, admin_headers
 
+from GeneralUtils.decorators import request_procedure
 
+
+@request_procedure
 def get_token_salt(username):
     url = f'https://{astra_id}-europe-west1.apps.astra.datastax.com/api/rest/v2/keyspaces/{keyspace}/{table_users}/{username}?fields=token-salt'
     return requests.get(url, headers=admin_headers)
