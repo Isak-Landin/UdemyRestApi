e646bbbbd8995049755099d07c56cf51f3c4ab17
Isak-Landin <utbisalan@gmail.com>
2023-07-30 00:57:51 +0200

    Update update_user_salt.py

--- database_modules/users_table/update_user_salt.py
+++ database_modules/users_table/update_user_salt.py
@@ -2,7 +2,10 @@ import requests
 import bcrypt
 from database_modules.localconfig import astra_id, table_users, keyspace
 
+from GeneralUtils.decorators import request_procedure
 
+
+@request_procedure
 def update_salt(username, salt):
     url = f'{astra_id}-europe-west1.apps.astra.datastax.com/api/rest/v1/keyspaces/{keyspace}/tables/{table_users}/rows/{username}'
 
