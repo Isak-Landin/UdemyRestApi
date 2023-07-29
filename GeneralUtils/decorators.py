551c50677eef76d3490336303fc2d9a57a65bf16
Isak-Landin <utbisalan@gmail.com>
2023-07-30 00:58:10 +0200

    Update decorators.py

--- GeneralUtils/decorators.py
+++ GeneralUtils/decorators.py
@@ -1,6 +1,8 @@
 from functools import wraps
 import traceback
 
+import requests
+
 def standard_procedure(function):
     @wraps(function)
     def standard_exception_return_event(*args, **kwargs):
@@ -14,4 +16,14 @@ def standard_procedure(function):
 
 def request_procedure(function):
     @wraps(function)
-    def standar
+    def standard_exception_return_event(*args, **kwargs):
+        try:
+            request_response: requests.Response = function(*args, **kwargs)
+            request_response.raise_for_status()
+
+            return request_response
+        except Exception as e:
+            print(traceback.format_exc())
+            raise e
+
+    return standard_exception_return_event
