6c3d96960337d4633e8161cd85721d084d9651b6
Isak-Landin <utbisalan@gmail.com>
2023-07-30 23:25:13 +0200

    Update add_error.py

--- database_modules/errors/add_error.py
+++ database_modules/errors/add_error.py
@@ -1,4 +1,12 @@
+import time
+
 import requests
+
+from GeneralUtils.decorators import request_procedure
+
 from database_modules.localconfig import astra_id, table_errors, admin_headers, keyspace
 
 
+@request_procedure
+def add_error(error):
+    time_now = int(time.time())
