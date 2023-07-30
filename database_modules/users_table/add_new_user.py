99519f240c52137c4cebd66ba1b50726e80ae77d
Isak-Landin <utbisalan@gmail.com>
2023-07-30 23:25:34 +0200

    Update add_new_user.py

--- database_modules/users_table/add_new_user.py
+++ database_modules/users_table/add_new_user.py
@@ -3,3 +3,9 @@ from database_modules.localconfig import astra_id, admin_headers, table_users, k
 
 from GeneralUtils.decorators import request_procedure
 
+
+@request_procedure
+def add_user(body) -> None:
+    url = f'https://{astra_id}-europe-west1.apps.astra.datastax.com/api/rest/v1/keyspaces/{keyspace}/tables/{table_users}/rows'
+    requests.post(url, headers=admin_headers, data=body)
+
